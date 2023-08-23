7fa379fa9c1f5bc9c0e527a58f6e820d3ca4c090
Quincy Miller <quibbles007@gmail.com>
2023-08-23 12:09:55 -0400

    Revert "Topics/qrm/10 firstunittests"

--- landscape-architecture.WebAPI/landscape-architecture.WebAPI/Services/ObjectToTopoServiceFacade.cs
+++ landscape-architecture.WebAPI/landscape-architecture.WebAPI/Services/ObjectToTopoServiceFacade.cs
@@ -51,8 +51,7 @@ namespace landscape_architecture.WebAPI.Services
             }
             return new TopoDTO();
         }
-
-        [DllImport("ConversionScripts.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+        [DllImport(@"C:\Users\parke\OneDrive\Desktop\Code\landscape-AR\landscape-architecture.WebAPI\x64\Debug\ConversionScripts.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
         public static extern bool objectToTopo(ref InputParams inputParams);
     }
 }
