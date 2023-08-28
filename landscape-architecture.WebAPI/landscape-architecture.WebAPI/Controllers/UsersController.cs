7349c037e34ab9ff72c15805b358e320339b0c94
Quincy Miller <quibbles007@gmail.com>
2023-08-28 13:15:00 -0400

    fleshed out user controller

--- landscape-architecture.WebAPI/landscape-architecture.WebAPI/Controllers/UsersController.cs
+++ landscape-architecture.WebAPI/landscape-architecture.WebAPI/Controllers/UsersController.cs
@@ -9,17 +9,48 @@ namespace landscape_architecture.WebAPI.Controllers
     public class UsersController : ControllerBase
     {
         readonly ILogger<UsersController> _logger;
-        public UsersController(ILogger<UsersController> logger) 
+
+        readonly IUsersService _usersService;
+        public UsersController(ILogger<UsersController> logger, IUsersService usersService)
         {
             _logger = logger;
+            _usersService = usersService;
+        }
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public ActionResult<List<UserDTO>> GetAllUsers()
+        {
+            return _usersService.GetAllUsers();
+        }
+
+        [HttpGet]
+        [Route("GetUserById")]
+        public ActionResult<UserDTO> GetUserById(int id)
+        {
+            return _usersService.GetUserById(id);
+        }
+
+        [HttpPut]
+        [Route("AddUser")]
+        public void AddUser(UserDTO user)
+        {
+            _usersService.AddUser(user);
+        }
+
+        [HttpPost]
+        [Route("UpdateUser")]
+        public void UpdateUser(UserDTO user)
+        {
+            _usersService.UpdateUser(user);
+        }
+
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public void DeleteUser(int id)
+        {
+            _usersService.DeleteUserById(id);
         }
     }
 
-/*    [HttpGet]
-    [Route("GetUsers")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public List<ActionResult<UserDTO>> GetAllUsers()
-    {
-
-    }*/
 }
