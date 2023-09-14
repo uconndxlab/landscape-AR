43ff9e1090ab1a6e4eae0e564c4c4943bb64098f
jonathanameri <jonathan.ameri@uconn.edu>
2023-09-14 09:38:25 -0400

    barebones file upload UI

--- landscape-architecture/src/App.vue
+++ landscape-architecture/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <FileUpload />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import FileUpload from '@/components/FileUpload.vue'
 </script>
